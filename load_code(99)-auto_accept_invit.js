6e1a40c137f3f5f13aacace8827d4d51b612d389
Thomas R <33552864+8Thomas8@users.noreply.github.com>
2019-02-01 23:44:28 +0100

    Update load_code(99)-auto_accept_invit.js

--- load_code(99)-auto_accept_invit.js
+++ load_code(99)-auto_accept_invit.js
@@ -1,22 +1,12 @@
 //Auto accept les invitations de groupe des personnages dans my_characters
-
 var my_characters = ["Lokda", "Tankda", "Healda"]
 
-
-
 setInterval(function () {
-
     //Si le perso n'est pas dans un groupe
-
     if (!character.party) {
-
-        my_characters.forEach(function (selected_character) {
-
-         accept_party_invite(selected_character);
-
+       my_characters.forEach(function (selected_character) {
+        accept_party_invite(selected_character);
         });
-
     }
-
 }, 15000); // Loops every 15 seconds
 
